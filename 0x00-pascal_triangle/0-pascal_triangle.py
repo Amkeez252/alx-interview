81603cd4c75be592e517a61950664a0cc9b0aa44
Amkeez252 <auwalamalik@gmail.com>
2023-06-29 22:42:12 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
+"""Pascal triangle"""
+
+
 def pascal_triangle(n):
- 
-    for line in range(1, n + 1):
-        C = 1; # used to represent C(line, i)
-        for i in range(1, line + 1):
-             
-            # The first value in a
-            # line is always 1
-            print(C, end = " ");
-            C = int(C * (line - i) / i);
-        print("");
- 
-# Driver code
-n = 5;
-printPascal(n);
+
+    if n <= 0:
+        return []
+
+    
+    """ initialize an empty resulting array """
+    pascal = [[] for idx in range(n)]
+
+    for li in range(n):
+        for col in range(li+1):
+            if(col < li):
+                if(col == 0):
+                    """ the first column is always set to 1 """
+                    pascal[li].append(1)
+                else:
+                    pascal[li].append(pascal[li-1][col] + pascal[li-1][col-1])
+            elif(col == li):
+                """ the diagonal is always set to 1 """
+                pascal[li].append(1)
+
+    return pascal
