f23db47cf236ef207476e1df833827a2b381c2d8
Amkeez252 <auwalamalik@gmail.com>
2023-08-23 07:21:20 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
+""" Making change module."""
+
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-
-    # Initialize a list to store the minimum number of coins needed for each value
+    # Initialize a list to store the minimum number of coins needed for each amount
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-
-    # Update dp array using dynamic programming
+    # Iterate through each coin value and update the dp list
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[1] = min(dp[1], dp[1 - coin] + 1)
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     if dp[total] == float('inf'):
         return -1
