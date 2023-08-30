25cab8baa69d251fd2bf81384f832c320700da68
Amkeez252 <auwalamalik@gmail.com>
2023-08-30 00:54:00 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 """Island Perimeter."""
 
-from typing import List
 
-
-def island_perimeter(grid: List) -> int:
+def island_perimeter(grid):
     """
     Island perimeter.
 
     Args:
-        grid (list): list of cells
+        grid.
     Returns:
         Returns int, the number of the perimeter.
     """
     if not grid:
         return 0
 
-    perimeter: int = 0
-    rows, cols = len(grid), len(grid[0])
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
 
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
                 perimeter += 4
 
-                # Check neighboring cells to subtract shared sides
+                # Check neighbors (up, down, left, right) and subtract 1 for each adjacent land cell
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2
+                    perimeter -= 1
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    perimeter -= 1
                 if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2
+                    perimeter -= 1
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    perimeter -= 1
 
     return perimeter
