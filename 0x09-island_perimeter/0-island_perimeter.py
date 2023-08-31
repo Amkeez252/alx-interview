edc71a2acb56775a7e0608f3f632cdb25a3e0b01
Amkeez252 <auwalamalik@gmail.com>
2023-08-31 14:26:28 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -24,6 +24,7 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
 
+
                 # Check neighbors (up, down, left, right) and subtract 1 for each adjacent land cell
                 if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 1
