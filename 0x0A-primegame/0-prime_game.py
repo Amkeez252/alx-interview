2a889911049cb079a3158990a1e034aaf1ba4475
Amkeez252 <auwalamalik@gmail.com>
2023-09-06 23:24:56 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,31 +2,34 @@
 """ Prime Game """
 
 def isWinner(x, nums):
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def get_primes(n):
+    def sieve(n):
         primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
+        sieve = [True] * (n + 1)
+        for p in range(2, n + 1):
+            if sieve[p]:
+                primes.append(p)
+                for i in range(p * p, n + 1, p):
+                    sieve[i] = False
         return primes
 
-    def play_game(n):
-        primes = get_primes(n)
-        if len(primes) % 2 == 0:
-            return "Ben"
-        else:
-            return "Maria"
+    def canWin(n, primes):
+        if n in primes:
+            return True
+        for prime in primes:
+            if n % prime == 0:
+                return True
+        return False
 
-    winners = [play_game(n) for n in nums]
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
+    maria_wins = 0
+    ben_wins = 0
+
+    primes = sieve(max(max(nums), 10000))
+
+    for n in nums:
+        if canWin(n, primes):
+            maria_wins += 1
+        else:
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
@@ -34,3 +37,4 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
+
